cf3b997a95804b5c3763354d943e7735f490d428
krupasebastian <krupasebastian6@wp.pl>
2023-11-10 14:23:51 +0100

    przemiana

---
+++ bip-app/src/components/MyAwesomeComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Price: {{state.price}}$</h1>
+    <button @click="makeOrder">Make next order</button>
+    <p>Quantity: {{state.quantity}}</p>
+    <p>Order total price: {{state.totalPrice}}</p>
+    <p>Tax: {{state.tax}}</p>
+  </div>
+  <!--<div>
+    <h1>Price: {{price}}$</h1>
+    <button @click="makeOrder">Make next order</button>
+    <p>Quantity: {{quantity}}</p>
+    <p>Order total price: {{totalPrice}}</p>
+    <p>Tax: {{tax}}</p>
+  </div>-->
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  name: "MyAwesomeComponent",
+  setup() {
+    const state = reactive({
+      quantity: 0,
+      price: 100,
+      totalPrice: 0,
+      tax: 0
+    });
+  
+    function makeOrder() {
+      state.quantity++;
+      state.totalPrice = state.quantity * state.price;
+      state.tax = state.totalPrice * 0.23;
+    }
+
+    return {makeOrder, state };
+  }
+};
+</script>
